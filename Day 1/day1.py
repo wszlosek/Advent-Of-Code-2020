4d44191ab8e443c0b63dea907f866fff51e02823
Wojciech Szlosek <wojciech185sz@gmail.com>
2020-12-17 15:15:36 +0100

    update day 1 (print)

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -2,24 +2,24 @@ def part1(lines):
     for a in lines:
         for b in lines:
             if (int(a) + int(b) == 2020):
-                print(f"Part 1: {int(a) * int(b)}")
-                return
+                return (int(a) * int(b))
+
 
 def part2(lines):
     for a in lines:
         for b in lines:
             for c in lines:
                 if(int(a) + int(b) + int(c) == 2020):
-                    print(f"Part 2: {int(a) * int(b) * int(c)}")
-                    return
+                    return (int(a) * int(b) * int(c))
 
 
 def main():
     with open("input.txt") as f:
         lines = f.read().splitlines()
-        
-    part1(lines)
-    part2(lines)
+
+
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
 
 
 if __name__ == "__main__":
